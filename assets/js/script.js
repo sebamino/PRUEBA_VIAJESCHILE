12d8d00033871deb4d6f118b0a38238cfac8b8ee
sebamino <sebastian.mino03@inacapmail.cl>
2023-05-17 12:12:57 -0400

    Carga de prueba: Viajes Chile By SMINOG

---
+++ assets/js/script.js
@@ -0,0 +1,6 @@
+// Mesaje generado al momento de enviar el mensaje, al precionar el boton contactar
+$(document).ready(function() {
+    $("#btn-contactar").click(function() {
+        alert("Felicidades: El correo fue enviado correctamente.");
+    });
+});
